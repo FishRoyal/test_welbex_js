f75a8669a8c6ba0ba3ec54806a5a2d1468fe7525
FishRoyal <dibelousov2000@gmail.com>
2023-03-11 01:41:59 +0300

    format functions

--- index.js
+++ index.js
@@ -2,10 +2,28 @@ const inputEl = document.querySelector('input');
 const buttonEl = document.querySelector('button');
 const timerEl = document.querySelector('span');
 
+
+const formatToTwoSymbols = (value) => {
+    const valStr = value.toString()
+    if(valStr.length === 1) return `0${value}`;
+    
+    return valStr;
+}
+
+const formatTime = (seconds) => {
+    const h = formatToTwoSymbols(Math.floor(seconds / 3600));
+    const m = formatToTwoSymbols(Math.floor((seconds - h * 3600) / 60));
+    const s = formatToTwoSymbols(Math.floor(seconds - h * 3600 - m * 60));
+
+    return `${h}:${m}:${s}}`
+}
+
 // Напишите реализацию createTimerAnimator
 // который будет анимировать timerEl
 const createTimerAnimator = () => {
-  return (seconds) => {};
+  return (seconds) => {
+
+  };
 };
 
 const animateTimer = createTimerAnimator();
